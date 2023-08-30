583e5ef44c88de0ad7ae1744619e59a9e7638653
Aejkatappaja <joverjlj@gmail.com>
2023-08-30 22:29:04 +0200

    rework: category type

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -3,5 +3,5 @@ import { IProduct } from 'types/product.type';
 
 export interface ICategory extends Document {
   name: string;
-  products: Types.Array<IProduct[]>;
+  products: Types.Array<IProduct>;
 }
