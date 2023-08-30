0ebbbbcb17cfb2b7615c8c9827ea21c562c9aa3d
Aejkatappaja <joverjlj@gmail.com>
2023-08-30 23:14:34 +0200

    fix: clean import syntax instead of require

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import morgan from 'morgan';
 import config from './config/env.config';
 import express, { Express, Request, Response } from 'express';
 
+require('./config/db.config');
+
 const app: Express = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors({ credentials: true }));
 
 const server = http.createServer(app);
+
+import productRouter from './routes/product';
+
+app.use(productRouter);
 
 app.get('/', (req: Request, res: Response) => {
   return res.send('Welcome to my API !');
