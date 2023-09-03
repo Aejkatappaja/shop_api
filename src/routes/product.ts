b3b241c5c8969318b607fe83f9615a5f18d40159
Aejkatappaja <joverjlj@gmail.com>
2023-09-03 22:26:40 +0200

    add: findByID product route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,8 +1,10 @@
+import { getProductById } from '../controllers/product/find.controller';
 import { createProduct } from '../controllers/product/create.controller';
 import express from 'express';
 
 const productRouter = express.Router();
 
 productRouter.post('/product/create', createProduct);
+productRouter.get('/product/:id', getProductById);
 
 export default productRouter;
