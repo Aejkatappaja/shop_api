f58be52993131744756667b1e9781158ad080aec
Aejkatappaja <joverjlj@gmail.com>
2023-09-03 22:25:56 +0200

    add: find product controller

---
+++ src/controllers/product/find.controller.ts
@@ -0,0 +1,21 @@
+import { IProduct } from '../../types/product.type';
+import { Request, Response } from 'express';
+import productService from '../../services/product.service';
+
+export const getProductById = async (
+  req: Request,
+  res: Response,
+): Promise<Response<IProduct, Record<string, unknown>>> => {
+  const productId = req.params.id;
+  try {
+    const product: IProduct = await productService.getProductId(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    } else {
+      return res.status(200).send({ product });
+    }
+  } catch (error: unknown) {
+    return res.status(500).send({ message: 'Internal server error' });
+  }
+};
