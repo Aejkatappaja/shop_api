938c3606e09d4847fce3a1ebf252be75b035c2ef
Aejkatappaja <joverjlj@gmail.com>
2023-09-04 22:42:26 +0200

    add: list transaction controller init

---
+++ src/controllers/transaction/list.controller.ts
@@ -0,0 +1,13 @@
+// import { Request, Response } from 'express';
+// import { ITransaction } from '../../types/transaction.type';
+
+// export const getAllTransactions = async (
+//   req: Request,
+//   res: Response,
+// ): Promise<Response<ITransaction, Record<string, unknown>>> => {
+//   try {
+//   } catch (error: unknown) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// };
