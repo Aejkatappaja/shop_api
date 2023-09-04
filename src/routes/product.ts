1ede297596e141745581fd37155447fb3574b581
Aejkatappaja <joverjlj@gmail.com>
2023-09-04 22:43:49 +0200

    add: get stock route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,12 @@
+import express from 'express';
 import { getProductById } from '../controllers/product/find.controller';
 import { createProduct } from '../controllers/product/create.controller';
-import express from 'express';
+import { getProducts } from '../controllers/product/list.controller';
 
 const productRouter = express.Router();
 
 productRouter.post('/product/create', createProduct);
-productRouter.get('/product/:id', getProductById);
+productRouter.get('/product/stock', getProducts);
+productRouter.get('/product/:id(\\d+)', getProductById); //add (\\d+) regexp to secure route with only number as passed params.
 
 export default productRouter;
