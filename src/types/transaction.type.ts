41c21c7caf4293ee2ee62d068081355b67304e51
Aejkatappaja <joverjlj@gmail.com>
2023-10-08 21:11:27 +0200

    fix(ITransaction): customer type

--- src/types/transaction.type.ts
+++ src/types/transaction.type.ts
@@ -4,7 +4,7 @@ import { IUser } from './user.type';
 
 export interface ITransaction extends Document {
   order: Types.Array<IProduct>;
-  customer: Types.Array<IUser>;
+  customer: IUser;
   total: number;
   date: Date;
 }
