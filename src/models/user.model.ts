0672114cb31837f23ef754b9078bddeea1f6f86a
Aejkatappaja <joverjlj@gmail.com>
2023-10-08 21:48:23 +0200

    add(user): address type

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Model } from 'mongoose';
+import mongoose, { Model, Schema } from 'mongoose';
 import { IUser } from 'types/user.type';
 
 export interface IUsertModel extends IUser, Document {}
@@ -7,9 +7,10 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   role: { type: String, enum: ['Customer', 'Admin'], required: true, default: 'Customer' },
-  email: { type: String, required: true },
+  address: { type: Schema.Types.Mixed, required: false },
+  email: { type: String, required: true, unique: true },
   avatar: { type: String, required: false },
-  created: { type: Date, required: true, default: Date.now() },
+  createdAt: { type: Date, required: true, default: Date.now() },
 });
 
 const User: Model<IUser> = mongoose.model<IUser>('User', userSchema);
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,10 +1,20 @@
-export type Roles = {
-  role: 'customer' | 'admin';
+export type RolesType = {
+  role: 'Customer' | 'Admin';
+};
+
+export type AddressType = {
+  country: string;
+  city: string;
+  street_name: string;
+  street_number: string;
+  zip_code: string;
+  additional_information: string;
 };
 export interface IUser extends Document {
   firstName: string;
   lastName: string;
-  role: Roles;
+  role: RolesType;
+  address: AddressType;
   email: string;
   avatar: string;
   created: Date;
