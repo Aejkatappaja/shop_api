9b03ee782d0a3f79c2bfdc60d676d996d05eb118
Aejkatappaja <joverjlj@gmail.com>
2023-10-08 21:10:28 +0200

    fix(route): product/:id

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { getProductById } from '../controllers/product/find.controller';
 import { createProduct } from '../controllers/product/create.controller';
 import { getProducts } from '../controllers/product/list.controller';
+import { isAdmin } from '../middlewares/admin.middleware';
 
 const productRouter = express.Router();
 
-productRouter.post('/product/create', createProduct);
+productRouter.post('/product/create', isAdmin, createProduct);
 productRouter.get('/product/stock', getProducts);
-productRouter.get('/product/:id(\\d+)', getProductById); //add (\\d+) regexp to secure route with only number as passed params.
+productRouter.get('/product/:id', getProductById); //add (\\d+) regexp to secure route with only number as passed params.
 
 export default productRouter;
