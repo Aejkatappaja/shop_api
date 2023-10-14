c81ad634da8eb5b5da0e29777396420e4c77feda
Aejkatappaja <joverjlj@gmail.com>
2023-10-14 22:34:14 +0200

    refact(product_service): try catch blocks added

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,42 +2,78 @@ import Product from '../models/product.model';
 import { IProduct } from '../types/product.type';
 
 export const Create = async (productInfos: IProduct): Promise<IProduct> => {
-  const newProduct = new Product(productInfos);
-  await newProduct.save();
-  return newProduct;
+  try {
+    const newProduct = new Product(productInfos);
+    await newProduct.save();
+    return newProduct;
+  } catch (error) {
+    console.error('Error during product creation process :', error);
+    throw error;
+  }
 };
 
 export const AlreadyExists = async (productInfos: IProduct): Promise<IProduct | boolean> => {
-  const existingProduct = await Product.findOne({ name: productInfos.name });
-  if (existingProduct) {
-    return true;
+  try {
+    const existingProduct = await Product.findOne({ name: productInfos.name });
+    if (existingProduct) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error('Error during product already exists process :', error);
+    throw error;
   }
-  return false;
 };
 
 export const WrongQuantity = async (productInfos: IProduct): Promise<IProduct | boolean> => {
-  if (productInfos.quantity <= 0) {
-    return true;
+  try {
+    if (productInfos.quantity <= 0) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error('Error during quantity verification input process :', error);
+    throw error;
   }
-  return false;
 };
 
 export const MissingInfos = async (productInfos: IProduct): Promise<IProduct | boolean> => {
-  if (
-    !productInfos.brand ||
-    !productInfos.name ||
-    !productInfos.price ||
-    !productInfos.quantity ||
-    !productInfos.description
-  ) {
-    return true;
+  try {
+    if (
+      !productInfos.brand ||
+      !productInfos.name ||
+      !productInfos.price ||
+      !productInfos.quantity ||
+      !productInfos.description
+    ) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error('Error during infos verification process :', error);
+    throw error;
   }
-  return false;
 };
 
-export const getProductId = (id: string): Promise<IProduct> => Product.findById(id);
+export const getProductId = async (id: string): Promise<IProduct> => {
+  try {
+    const product = await Product.findById(id);
+    return product;
+  } catch (error) {
+    console.error('Error during fetching productId process :', error);
+    throw error;
+  }
+};
 
-export const getAllProducts = async (): Promise<IProduct[]> => await Product.find().sort({ brand: 1 });
+export const getAllProducts = async (): Promise<IProduct[]> => {
+  try {
+    const products = await Product.find().sort({ brand: 1 });
+    return products;
+  } catch (error) {
+    console.error('Error during infos verification process :', error);
+    throw error;
+  }
+};
 
 const productService = {
   Create,
