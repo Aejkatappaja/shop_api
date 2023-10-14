c52fb3a9b207e5b3689cef851d3bc1e207fa4e2f
Aejkatappaja <joverjlj@gmail.com>
2023-10-14 22:33:54 +0200

    refacto(isAdmin): try catch added

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const isAdmin = (req: Request & { user?: { role: string } }, res: Response, next: NextFunction): void => {
-  //   if (req.body.user && req.body.user.role === 'Admin') {
-  if (req.user && req.user.role === 'Admin') {
-    next();
-  } else {
-    res.status(403).json({ message: 'Access denied. You are not an admin.' });
+  try {
+    if (req.user && req.user.role === 'Admin') {
+      next();
+    } else {
+      res.status(403).json({ message: 'Access denied. You are not an admin.' });
+    }
+  } catch (error: unknown) {
+    console.error('error on credentials checking process', error);
+    throw error;
   }
+  //   if (req.body.user && req.body.user.role === 'Admin') {
 };
