2087f2efd8be6d629819ddd30355993a767c737f
Aejkatappaja <joverjlj@gmail.com>
2023-10-15 16:51:43 +0200

    refacto(login_controller): token & functions

--- src/controllers/user/login.controller.ts
+++ src/controllers/user/login.controller.ts
@@ -1,44 +1,31 @@
 import userLoginService from '../../database/services/user.login.service';
 
-import { IUser, IUserResponse } from '../../types/user.type';
+import { IUser } from '../../types/user.type';
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 export const userLogin = async (req: Request, res: Response): Promise<Response<IUser, Record<string, unknown>>> => {
   try {
     const userInfos: IUser = req.body;
 
     const missingUserInfos = await userLoginService.MissingInfos(userInfos);
-
-    const verifyUserExists = await userLoginService.VerifyUserExists(userInfos);
-
-    const userLogged = await userLoginService.Login(userInfos);
-
-    const verifiedPassword = await userLoginService.passwordVerification(userInfos, userLogged);
-
     if (missingUserInfos) {
       return res.status(400).json({ message: 'You need to provide all required informations' });
     }
 
+    const verifyUserExists = await userLoginService.VerifyUserExists(userInfos);
     if (verifyUserExists) {
       return res.status(400).json({ message: 'There is a problem with provided informations' });
     }
 
+    const userLogged = await userLoginService.Login(userInfos);
     if (!userLogged) {
       return res.status(400).json({ message: 'There is a problem with your account informations' });
     }
-    const token = jwt.sign({ role: userLogged.role }, 'secret', { expiresIn: '1d' });
 
+    const verifiedPassword = await userLoginService.passwordVerification(userInfos, userLogged);
     if (verifiedPassword) {
-      const { firstName, lastName, email, address, avatar } = userLogged;
-      const userResponse: IUserResponse = {
-        firstName,
-        lastName,
-        address,
-        email,
-        avatar,
-        token,
-      };
+      const createToken = await userLoginService.generateToken(userLogged);
+      const userResponse = await userLoginService.userResponse(userLogged, createToken);
       return res.status(200).json(userResponse);
     } else {
       return res.status(409).json({ message: 'You provided wrong informations' });
