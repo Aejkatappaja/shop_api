fe3dab9740ade74482393c436e76b2b192ac4e65
Aejkatappaja <joverjlj@gmail.com>
2023-10-15 16:53:17 +0200

    chore(admin_middleware):  logic

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -1,15 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export const isAdmin = (req: Request & { user?: { role: string } }, res: Response, next: NextFunction): void => {
   try {
-    if (req.user && req.user.role === 'Admin') {
-      next();
-    } else {
-      res.status(403).json({ message: 'Access denied. You are not an admin.' });
+    const token = req.headers.authorization;
+    if (!token) {
+      return;
     }
+
+    const accessToken = token.split(' ')[1];
+    console.log(accessToken);
+
+    jwt.verify(accessToken, 'secret', (err, decoded: JwtPayload | string) => {
+      if (err) {
+        return res.status(401).json({ message: 'Token is invalid' });
+      }
+
+      if (typeof decoded === 'string') {
+        return;
+      } else {
+        const userRole = (decoded as JwtPayload).role;
+
+        if (userRole !== 'Admin') {
+          return res.status(403).json({ message: 'Insufficient permissions' });
+        }
+      }
+
+      next();
+    });
   } catch (error: unknown) {
     console.error('error on credentials checking process', error);
     throw error;
   }
-  //   if (req.body.user && req.body.user.role === 'Admin') {
 };
