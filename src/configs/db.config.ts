3d15266ce4e5e82174c2606fac7878c6b734daf0
Aejkatappaja <joverjlj@gmail.com>
2023-10-15 22:12:04 +0200

    refacto(config): db env

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+import config from './env.config';
 
-import mongoose from "mongoose";
-const mongoURL = process.env.MONGODB_URL;
+import mongoose from 'mongoose';
+const { MONGODB_URL } = config;
 
 mongoose.Promise = Promise;
-mongoose.connect(mongoURL);
-mongoose.connection.on("error", (error: unknown) => console.log(error));
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on('error', (error: unknown) => console.log(error));
 
 module.exports = mongoose;
--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,6 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 
 export default {
   port: process.env.PORT || 4000,
+  MONGODB_URL: process.env.MONGODB_URL,
+  SECRET: process.env.SECRET,
 };
