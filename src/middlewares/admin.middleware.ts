21d66b1f26696fd52c1468953fbd2981a781c08c
Aejkatappaja <joverjlj@gmail.com>
2023-10-15 22:18:51 +0200

    refacto(admin_middleware): user object

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -1,17 +1,19 @@
+import config from '../configs/env.config';
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import { IUserResponse } from '../types/user.type';
 
-export const isAdmin = (req: Request & { user?: { role: string } }, res: Response, next: NextFunction): void => {
+export const isAdmin = (req: Request & { user?: IUserResponse }, res: Response, next: NextFunction): void => {
   try {
+    const { SECRET } = config;
     const token = req.headers.authorization;
     if (!token) {
       return;
     }
 
     const accessToken = token.split(' ')[1];
-    console.log(accessToken);
 
-    jwt.verify(accessToken, 'secret', (err, decoded: JwtPayload | string) => {
+    jwt.verify(accessToken, SECRET, (err, decoded: JwtPayload | string) => {
       if (err) {
         return res.status(401).json({ message: 'Token is invalid' });
       }
@@ -19,9 +21,14 @@ export const isAdmin = (req: Request & { user?: { role: string } }, res: Respons
       if (typeof decoded === 'string') {
         return;
       } else {
-        const userRole = (decoded as JwtPayload).role;
+        const user = (decoded as JwtPayload).user;
 
-        if (userRole !== 'Admin') {
+        if (user) {
+          delete user.password;
+          req.user = user;
+        }
+
+        if (user.role !== 'Admin') {
           return res.status(403).json({ message: 'Insufficient permissions' });
         }
       }
