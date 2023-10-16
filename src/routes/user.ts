5a1de60ad6f8aa4f5eaf1d618f722b1d2aab7fc2
Aejkatappaja <joverjlj@gmail.com>
2023-10-16 21:43:15 +0200

    refacto(user_schema): _id

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,12 +3,13 @@ import { Request, Response } from 'express';
 import { isAdmin } from '../middlewares/admin.middleware';
 import { userRegister } from '../controllers/user/register.controller';
 import { userLogin } from '../controllers/user/login.controller';
+import { IUser } from 'types/user.type';
 
 const userRouter = express.Router();
 
-userRouter.get('/user/admin-only', isAdmin, (req: Request, res: Response) => {
-  const user = req;
-  console.log('user', user);
+userRouter.get('/user/admin-only', isAdmin, (req: Request & { user?: IUser }, res: Response) => {
+  // const user = req;
+  // console.log('userzzzzz', user.user);
 
   return res.json({ message: 'Welcome, admin!' });
 });
