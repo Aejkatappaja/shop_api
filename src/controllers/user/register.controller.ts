f8d4e745230c74587fcd45ecd39b306499eb1c7a
Aejkatappaja <joverjlj@gmail.com>
2023-10-19 00:42:54 +0200

    refacto(register_controller): imports ans fn names

--- src/controllers/user/register.controller.ts
+++ src/controllers/user/register.controller.ts
@@ -1,29 +1,30 @@
 import { encryptPassword } from '../../utils/encrypt-password';
-import userRegisterService from '../../database/services/user.register.service';
+import user_register_services from '../../database/services/user/register';
 import { IUser } from '../../types/user.type';
 import { Request, Response } from 'express';
 
 export const userRegister = async (req: Request, res: Response): Promise<Response<IUser, Record<string, unknown>>> => {
   try {
     const userInfos: IUser = req.body;
+    const { email } = userInfos;
 
-    const missingUserInfos = await userRegisterService.MissingInfos(userInfos);
+    const missingRequiredInformations = await user_register_services.missingRequiredInformations(userInfos);
 
-    if (missingUserInfos) {
+    if (missingRequiredInformations) {
       return res.status(400).json({ message: 'You need to provide all required informations' });
     }
 
-    const emailTaken = await userRegisterService.EmailTaken(userInfos);
-    if (emailTaken) {
+    const emailAvailable = await user_register_services.emailAvailable(email);
+    if (!emailAvailable) {
       return res.status(409).json({ message: 'Email already used' });
     } else {
-      const validPassword = await userRegisterService.ValidPassword(userInfos);
+      const correctPasswordFormat = await user_register_services.passwordFormatVerification(userInfos.password);
       const passwordEncrypt = await encryptPassword(userInfos.password);
-      if (validPassword) {
+      if (correctPasswordFormat) {
         userInfos.password = passwordEncrypt;
       }
 
-      await userRegisterService.Create(userInfos);
+      await user_register_services.newUserCreation(userInfos);
       return res.status(200).json({ message: 'Account successfully created!' });
     }
   } catch (error: unknown) {
