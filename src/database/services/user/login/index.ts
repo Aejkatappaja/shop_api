7f07278d1adb93fe92cf023c863612426270b4b0
Aejkatappaja <joverjlj@gmail.com>
2023-10-18 23:46:56 +0200

    delete(generate_token): user login services

--- src/database/services/user/login/index.ts
+++ src/database/services/user/login/index.ts
@@ -1,12 +1,10 @@
 import { userExists } from './existing-user';
-import { generateToken } from './generate-token';
 import { passwordVerification } from './password-verification';
 import { userSuccessfullyRetrieved } from './retrieve-user';
 import { missingProvidedInformations } from './verifying-infos';
 
 const user_login_services = {
   userExists,
-  generateToken,
   missingProvidedInformations,
   userSuccessfullyRetrieved,
   passwordVerification,
