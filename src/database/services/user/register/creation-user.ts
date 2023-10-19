d659c928b9bfd15a35b16c6bbf2bdea2695591b6
Aejkatappaja <joverjlj@gmail.com>
2023-10-19 23:22:09 +0200

    refacto(user_services):  error handling

--- src/database/services/user/register/creation-user.ts
+++ src/database/services/user/register/creation-user.ts
@@ -3,9 +3,13 @@ import { IUser } from '../../../../types/user.type';
 
 export const newUserCreation = async (userInfos: IUser): Promise<IUser | null> => {
   try {
-    const newUser = new User(userInfos);
-    await newUser.save();
-    return newUser;
+    if (!userInfos) {
+      return null;
+    } else {
+      const newUser = new User(userInfos);
+      await newUser.save();
+      return newUser;
+    }
   } catch (error: unknown) {
     console.error('Error creating user:', error);
     throw error;
