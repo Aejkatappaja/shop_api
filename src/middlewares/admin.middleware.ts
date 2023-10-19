2a500922412b2d6939f2f087dfdbf8e58262d16c
Aejkatappaja <joverjlj@gmail.com>
2023-10-19 22:17:02 +0200

    refacto(middleware): isAdmin handle !token

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -3,12 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from '../database/models/user.model';
 
-export const isAdmin = (req: Request, res: Response, next: NextFunction): void => {
+export const isAdmin = (req: Request, res: Response, next: NextFunction): void | Response => {
   try {
     const { SECRET } = config;
     const token = req.headers.authorization;
     if (!token) {
-      return;
+      if (!token) {
+        return res.status(401).json({ message: 'Token is missing' });
+      }
     }
 
     const accessToken = token.split(' ')[1];
@@ -22,10 +24,6 @@ export const isAdmin = (req: Request, res: Response, next: NextFunction): void =
         return;
       } else {
         const user = await User.findById((decoded as JwtPayload).id);
-
-        // if (user) {
-        //   req.user = user;
-        // }
 
         if (user.role !== 'Admin') {
           return res.status(403).json({ message: 'Insufficient permissions' });
