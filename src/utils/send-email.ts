cdc19d3a467dfb817249c6d9baf5e02099719b55
Aejkatappaja <joverjlj@gmail.com>
2023-11-04 21:57:01 +0100

    test(resend): send mail

---
+++ src/utils/send-email.ts
@@ -0,0 +1,21 @@
+// import { Resend } from 'resend';
+// import config from '../configs/env.config';
+
+// const { RESEND_API_KEY } = config;
+
+// const resend = new Resend(RESEND_API_KEY);
+
+// export async function sendEmail() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Frank <admin@Franks.burger.dev>',
+//       to: ['frank.j@sonup.fr'],
+//       subject: 'Hello World',
+//       html: '<strong>It works!</strong>',
+//     });
+
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
