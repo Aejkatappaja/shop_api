765edc80a956369de0222768cb66ba2ff0eb1e6f
Aejkatappaja <joverjlj@gmail.com>
2023-11-05 21:50:03 +0100

    feat(list.controller): add pagination

--- src/controllers/user/list.controller.ts
+++ src/controllers/user/list.controller.ts
@@ -2,12 +2,11 @@ import { Request, Response } from 'express';
 import { IUser } from '../../types/user.type';
 import user_get_services from '../../database/services/user/get';
 
-export const getAllUsers = async (
-  req: Request & { userId?: string },
-  res: Response,
-): Promise<Response<IUser, Record<string, unknown>>> => {
+export const getAllUsers = async (req: Request, res: Response): Promise<Response<IUser, Record<string, unknown>>> => {
+  const { page } = req.query;
+  const currentPage = Number(page);
   try {
-    const userList = await user_get_services.getUsersList();
+    const userList = await user_get_services.getUsersList(currentPage);
     if (userList) {
       return res.status(201).json(userList);
     } else return res.status(400).json({ message: 'Error during fetch user list process!' });
