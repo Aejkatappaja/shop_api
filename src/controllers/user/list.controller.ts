ac8d48b2be483578d705d6052239b73b0a958089
Aejkatappaja <joverjlj@gmail.com>
2023-11-07 22:29:52 +0100

    add(getAllUsers): query

--- src/controllers/user/list.controller.ts
+++ src/controllers/user/list.controller.ts
@@ -4,9 +4,14 @@ import user_get_services from '../../database/services/user/get';
 
 export const getAllUsers = async (req: Request, res: Response): Promise<Response<IUser, Record<string, unknown>>> => {
   const { page } = req.query;
+  let query = req.query.query as string;
   const currentPage = Number(page);
   try {
-    const userList = await user_get_services.getUsersList(currentPage);
+    if (Array.isArray(query)) {
+      query = query[0];
+    }
+
+    const userList = await user_get_services.getUsersList(currentPage, query);
     if (userList) {
       return res.status(201).json(userList);
     } else return res.status(400).json({ message: 'Error during fetch user list process!' });
