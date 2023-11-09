8a5c2f73e91c1a4817f0d1f880db01d2d9fb00f3
Aejkatappaja <joverjlj@gmail.com>
2023-11-09 16:52:24 +0100

    refacto(adminRouter): add get-user/:id and rename routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import { isAdmin } from '../middlewares/admin.middleware';
 import { getAllUsers } from '../controllers/user/list.controller';
 
+import { adminGetUser } from '../controllers/user/admin.find.controller';
+
 const adminRouter = express.Router();
 
 adminRouter.use(isAdmin);
 
-adminRouter.get('/users', getAllUsers);
+adminRouter.get('/admin/get-users', getAllUsers);
+adminRouter.get('/admin/get-user/:id', adminGetUser);
 
 export default adminRouter;
