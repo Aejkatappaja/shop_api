5b1ad0c26621e45059694c0a1c17848914fa04fd
Aejkatappaja <joverjlj@gmail.com>
2023-11-15 19:10:22 +0100

    add(routes_admin): access front dashboard verification

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { isAdmin } from '../middlewares/admin.middleware';
 import { getAllUsers } from '../controllers/user/list.controller';
-
 import { adminGetUser } from '../controllers/user/admin.find.controller';
+import { getDashboardAccess } from '../controllers/admin.dashboard-access.controller';
 
 const adminRouter = express.Router();
 
 adminRouter.use(isAdmin);
 
+adminRouter.get('/admin/dashboard-access', getDashboardAccess);
 adminRouter.get('/admin/get-users', getAllUsers);
 adminRouter.get('/admin/get-user/:id', adminGetUser);
 
