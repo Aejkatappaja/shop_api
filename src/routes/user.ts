fec374bb1148a3d62a25310baa27c036b09770c7
Aejkatappaja <joverjlj@gmail.com>
2023-11-15 19:09:26 +0100

    delete(routes_user): unused routes and comments

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,26 +1,15 @@
 import express from 'express';
-import { Request, Response } from 'express';
-import { isAdmin } from '../middlewares/admin.middleware';
 import { userRegister } from '../controllers/user/register.controller';
 import { userLogin } from '../controllers/user/login.controller';
-import { IUser } from 'types/user.type';
 import { verifiedUser } from '../middlewares/verified_user.middleware';
 import { getUser } from '../controllers/user/find.controller';
 import { deleteUser } from '../controllers/user/delete.controller';
 
 const userRouter = express.Router();
 
-userRouter.get('/user/admin-only', isAdmin, (req: Request & { user?: IUser }, res: Response) => {
-  // const user = req;
-  // console.log('userzzzzz', user.user);
-
-  return res.json({ message: 'Welcome, admin!' });
-});
-
 userRouter.post('/user/register', userRegister);
 userRouter.post('/user/login', userLogin);
 userRouter.get('/user', verifiedUser, getUser);
-// userRouter.get('/user/:id', isAdmin, AdminGetUser);
 userRouter.delete('/user/delete', verifiedUser, deleteUser);
 
 export default userRouter;
