a615a168fd17a1032df5e405158b86457f04298c
Aejkatappaja <joverjlj@gmail.com>
2023-11-16 21:54:36 +0100

    feat(update product): controller & admin route

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,6 +3,8 @@ import { isAdmin } from '../middlewares/admin.middleware';
 import { getAllUsers } from '../controllers/user/list.controller';
 import { adminGetUser } from '../controllers/user/admin.find.controller';
 import { getDashboardAccess } from '../controllers/admin.dashboard-access.controller';
+import { updateProduct } from '../controllers/product/update.controller';
+import fileUpload from 'express-fileupload';
 
 const adminRouter = express.Router();
 
@@ -11,5 +13,6 @@ adminRouter.use(isAdmin);
 adminRouter.get('/admin/dashboard-access', getDashboardAccess);
 adminRouter.get('/admin/get-users', getAllUsers);
 adminRouter.get('/admin/get-user/:id', adminGetUser);
+adminRouter.patch('/admin/update-product/:id', fileUpload(), updateProduct);
 
 export default adminRouter;
