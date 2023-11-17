4a441a2ccb1991b70d4dfbaf7b568193bd518b95
Aejkatappaja <joverjlj@gmail.com>
2023-11-17 17:32:21 +0100

    refacto(routes): swap admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -5,6 +5,8 @@ import { adminGetUser } from '../controllers/user/admin.find.controller';
 import { getDashboardAccess } from '../controllers/admin.dashboard-access.controller';
 import { updateProduct } from '../controllers/product/update.controller';
 import fileUpload from 'express-fileupload';
+import { createProduct } from '../controllers/product/create.controller';
+import { deleteProduct } from '../controllers/product/delete.controller';
 
 const adminRouter = express.Router();
 
@@ -14,5 +16,7 @@ adminRouter.get('/admin/dashboard-access', getDashboardAccess);
 adminRouter.get('/admin/get-users', getAllUsers);
 adminRouter.get('/admin/get-user/:id', adminGetUser);
 adminRouter.patch('/admin/update-product/:id', fileUpload(), updateProduct);
+adminRouter.post('/admin/product/create', createProduct);
+adminRouter.delete('/admin/product/delete/:id', deleteProduct);
 
 export default adminRouter;
