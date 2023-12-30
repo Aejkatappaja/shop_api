a0b3dfa7a8d6d4bc408aaca4116423ffdf16f293
Aejkatappaja <joverjlj@gmail.com>
2023-12-30 20:46:55 +0100

    delete(db_services_retrieve-all-users): remove commented code

--- src/database/services/user/get/retrieve-all-users.ts
+++ src/database/services/user/get/retrieve-all-users.ts
@@ -1,18 +1,3 @@
-// import User from '../../../models/user.model';
-// import { IUser } from '../../../../types/user.type';
-
-// export const getUsersList = async (): Promise<IUser[] | null> => {
-//   try {
-//     const users = await User.find().select('-password -__v').sort({ createdAt: -1 });
-//     if (users) {
-//       return users;
-//     } else return null;
-//   } catch (error: unknown) {
-//     console.error('Error during fetching productId process :', error);
-//     throw error;
-//   }
-// };
-
 import User from '../../../models/user.model';
 import { IUser } from '../../../../types/user.type';
 
